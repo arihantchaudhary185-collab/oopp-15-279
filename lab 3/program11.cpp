255ee22de72246f4e03c437159bf2b23769719fc
arihantchaudhary185-collab <arihantchaudhary185@gmail.com>
2026-09-25 09:27:56 +0530

    new program

---
+++ lab 3/program11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+inline int add(int a, int b = 10)
+{
+    return a + b;
+}
+
+int add(int a, int b, int c)
+{
+    return a + b + c;
+}
+
+int multiply(int a, int b = 2)
+{
+    return a * b;
+}
+
+int main()
+{
+    cout << "Addition: " << add(5) << endl;
+    cout << "Addition of 3 numbers: " << add(5, 10, 15) << endl;
+    cout << "Multiplication: " << multiply(5) << endl;
+
+    return 0;
+}
